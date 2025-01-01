5f52da5f8da2a9c1e3bf4079ab0666e987d82d3b
blwatkins <blwatkins@users.noreply.github.com>
2025-01-01 11:44:01 -0600

    Complete transfer of Palette interface.

--- src/main/color/palette/palette.ts
+++ src/main/color/palette/palette.ts
@@ -17,12 +17,64 @@
 
 import { Discriminable, Discriminators } from 'discriminator';
 
+import { PaletteColor } from './palette-color';
+
 /**
  * A collection of {@link PaletteColor} objects.
  *
  * @category Palette
  */
 export interface Palette extends Discriminable {
+    /**
+     * The name of the palette.
+     */
+    readonly NAME: string;
+
+    /**
+     * The source of the palette.
+     */
+    readonly SOURCE?: string;
+
+    /**
+     * The source URL of the palette.
+     */
+    readonly SOURCE_URL?: string;
+
+    /**
+     * A flag indicating if the palette is a gradient.
+     */
+    readonly IS_GRADIENT: boolean;
+
+    /**
+     * The list of {@link PaletteColor} objects that compose the palette.
+     */
+    readonly COLORS: PaletteColor[];
+
+    /**
+     * A map of hex color strings to arrays of hex colors,
+     * where each color in the array passes the WCAG AA standard
+     * with normal and large size text when compared to the key.<br/>
+     * The only colors listed in this map should be the colors of the
+     * palette, black (#000000), and white (#FFFFFF).
+     */
+    readonly CONTRAST_MAP?: {
+        /**
+         * The colors in the palette that pass the WCAG AA standard when
+         * compared to black (#000000). This list should NOT contain
+         * white (#FFFFFF) unless it is a color listed in the palette.
+         */
+        readonly '#000000': string[];
+
+        /**
+         * The colors in the palette that pass the WCAG AA standard when
+         * compared to white (#FFFFFF). This list should NOT contain
+         * black (#000000) unless it is a color listed in the palette.
+         */
+        readonly '#FFFFFF': string[];
+
+        readonly [HEX: string]: string[];
+    };
+
     /**
      * @inheritDoc
      */
