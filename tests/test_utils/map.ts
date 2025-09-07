88acd81cf0692cca4ee06f639953642778e5e17f
blwatkins <blwatkins@users.noreply.github.com>
2025-09-07 14:18:11 -0500

    #302 Add unit test for BLACK_PALETTE_COLORS map.

---
+++ tests/test_utils/map.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024-2025 brittni and the polar bear LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { expect } from 'vitest';
+
+import { map } from "../../src";
+
+export function testStringMap<Type>(map: map.StringMap<Type>, expectedSize?: number): void {
+    expect(map).toBeTruthy();
+
+    const keys: string[] = Array.from(map.keys());
+    const values: Type[] = Array.from(map.values());
+
+    expect(values.length).toBe(keys.length);
+    expect(keys.length).toBe(map.size);
+
+    if (expectedSize !== undefined) {
+        if (expectedSize === 0) {
+            expect(keys.length).toBe(0);
+            expect(values.length).toBe(0);
+            expect(map.size).toBe(0);
+        } else if (expectedSize > 0) {
+            expect(keys.length).toBe(expectedSize);
+            expect(values.length).toBe(expectedSize);
+            expect(map.size).toBe(expectedSize);
+        } else if (expectedSize < 0) {
+            throw new Error(`Expected size cannot be negative: ${expectedSize}`);
+        }
+    } else {
+        expect(keys.length).toBeGreaterThan(0);
+        expect(values.length).toBeGreaterThan(0);
+        expect(map.size).toBeGreaterThan(0);
+    }
+}
