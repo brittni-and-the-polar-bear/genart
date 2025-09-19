c36978eb6e9577bc8411b43f747b2613df9af984
blwatkins <blwatkins@users.noreply.github.com>
2025-09-19 11:18:35 -0500

    #331 #302 Add new p5_context module and initial P5Context class.

--- src/p5_context/index.ts
+++ src/p5_context/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2025 brittni and the polar bear LLC.
+ * Copyright (C) 2023-2025 brittni and the polar bear LLC.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,23 +20,4 @@
  * SOFTWARE.
  */
 
-export * from './color';
-export * as color from './color';
-
-export * from './discriminator';
-export * as discriminator from './discriminator';
-
-export * from './map';
-export * as map from './map';
-
-export * from './p5_context';
-export * as p5_context from './p5_context';
-
-export * from './palette';
-export * as palette from './palette';
-
-export * from './palette_color';
-export * as palette_color from './palette_color';
-
-export * from './string';
-export * as string from './string';
+export * from './p5-context';
