cf348db81a8caaa6848dee8c348a761712acb40c
blwatkins <blwatkins@users.noreply.github.com>
2025-10-29 15:06:06 -0500

    #174 Add new color: rapunzel silver (#d2d3d5)

--- src/palette_color/gray/pc-d2d3d5.ts
+++ src/palette_color/gray/pc-d2d3d5.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024-2025 brittni and the polar bear LLC.
+ * Copyright (C) 2025 brittni and the polar bear LLC.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +20,34 @@
  * SOFTWARE.
  */
 
-export * from './gray-colors';
+import { ColorNames } from '../../color';
+import { Discriminators } from '../../discriminator';
 
-export * from './pc-3a3e40';
-export * from './pc-a3a3a3';
-export * from './pc-d2d3d5';
+import { ALL_PALETTE_COLORS } from '../all-colors';
+import { PaletteColor } from '../palette-color';
+
+import { GRAY_PALETTE_COLORS } from './gray-colors';
+
+/**
+ * <div class="color-block" style="background: #D2D3D5;">
+ *     <a href="https://coolors.co/d2d3d5" target="_blank" rel="noopener noreferrer">
+ *         <h2 class="color-block black-pass">rapunzel silver (#D2D3D5)</h2>
+ *     </a>
+ * </div>
+ *
+ * @since 2.0.0
+ *
+ * @category Gray
+ */
+export const PC_D2D3D5: PaletteColor = {
+    HEX: '#D2D3D5',
+    RGB: { R: 210, G: 211, B: 213 },
+    HSL: { H: 220, S: 3, L: 83 },
+    NAME: 'rapunzel silver',
+    LUMINANCE: 0.6509426552,
+    DISCRIMINATOR: Discriminators.PALETTE_COLOR
+};
+
+ALL_PALETTE_COLORS.setIfAbsent(PC_D2D3D5.HEX, PC_D2D3D5);
+GRAY_PALETTE_COLORS.setIfAbsent(PC_D2D3D5.HEX, PC_D2D3D5);
+ColorNames.addColor(PC_D2D3D5);
