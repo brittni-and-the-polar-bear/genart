ff389ffa16b101a888fa189d7eadb01aa1484915
blwatkins <blwatkins@users.noreply.github.com>
2025-12-07 15:55:16 -0600

    Update documentation in the graphics module.

--- src/sketch/graphics/graphics-context.ts
+++ src/sketch/graphics/graphics-context.ts
@@ -45,7 +45,11 @@ export class GraphicsContext extends Context {
     /**
      * The constructor for the GraphicsContext class.
      *
-     * @param config - The configuration for the context.
+     * @constructor
+     *
+     * @param config {ContextConfig} - The configuration for the context.
+     *
+     * @since 2.0.0
      */
     public constructor(config: ContextConfig) {
         super(config);
@@ -92,7 +96,7 @@ export class GraphicsContext extends Context {
     }
 
     /**
-     * @returns the p5.Graphics instance backing this context.
+     * @returns {p5.Graphics} The p5.Graphics instance backing this context.
      *
      * @since 2.0.0
      */
